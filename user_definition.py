dedede27d6d42bb22440c19cf4f9006855b06971
Shubham Thakur <shubhamthakur@Shubhams-MacBook-Air.local>
2022-03-16 16:57:11 -0700

    Pyspark DataFrame Homework

---
+++ user_definition.py
@@ -0,0 +1,16 @@
+air_quality_file = "data/epa_air_quality.csv"
+location_file = "data/epa_site_location.csv"
+
+#output 1.
+n = 10
+start_string = 'S'
+start_date = "2020-01-01"
+end_date = "2020-01-07"
+site_id_val = 60270022
+
+# output 2.
+# n = 5
+# site_id_val = 60710005
+# start_string = 'C'
+# start_date = "2020-05-01"
+# end_date = "2020-05-05"
